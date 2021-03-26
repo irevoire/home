9bebce4c9d41205caf80291209f51a2ff7b37340
tamo <irevoire@protonmail.ch>
2021-03-26 23:00:01 +0100

    makes my mapping more consistent

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_keymap('n', '<C-f>', ":Files<cr>", { noremap = true, silent = t
 
 -- lsp mappings
 vim.api.nvim_set_keymap('n', '<leader>gd', "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gtd', "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gi', "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gr', "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
 
@@ -11,7 +12,6 @@ vim.api.nvim_set_keymap('n', '<leader>r', "<cmd>lua vim.lsp.buf.rename()<cr>", {
 vim.api.nvim_set_keymap('n', '<leader>a', "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 --[[
-vim.api.nvim_set_keymap('n', '1gD', "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'g0', "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
 --]]
 
