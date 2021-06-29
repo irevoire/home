401d1f7a62050767bea316b939fa14b4f62cd717
Tamo <tamo@meilisearch.com>
2021-06-29 17:50:32 +0200

    add a mapping to search in the document symbols

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,14 +6,12 @@ vim.api.nvim_set_keymap('n', '<leader>gd', "<cmd>lua vim.lsp.buf.definition()<CR
 vim.api.nvim_set_keymap('n', '<leader>gtd', "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gi', "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gr', "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gs', "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>h', "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>r', "<cmd>lua vim.lsp.buf.rename()<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>a', "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
---[[
-vim.api.nvim_set_keymap('n', 'g0', "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
---]]
 
 -- Auto completion
 -- vim.api.nvim_set_keymap('i', '<C-n>', "pumvisible", { noremap = true, silent = true })
